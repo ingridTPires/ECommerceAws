6238d62cbf9d3b3812d79b61dcc580ed1679295b
IngridTPires <ingrid.teixeirap@gmail.com>
2024-04-14 16:57:27 -0300

    Capturar RequestId

--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -3,10 +3,15 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda
 export async function handler(event:APIGatewayProxyEvent,
     context: Context): Promise<APIGatewayProxyResult> {
     
-    const method = event.httpMethod;
+    const lambdaRequestId = context.awsRequestId
+    const apiRequestId = event.requestContext.requestId
+
+    console.log(`API Gateway RequestId: ${apiRequestId} - Lambda RequestId: ${lambdaRequestId}`)
+    
+    const method = event.httpMethod
     if(event.resource == "/products"){
         if(method === 'GET'){
-            console.log('GET');
+            console.log('GET')
 
             return {
                 statusCode: 200,
