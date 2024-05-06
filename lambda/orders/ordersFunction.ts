853bff93763438ce5afbd1240aa9152bdd24ef30
IngridTPires <ingrid.teixeirap@gmail.com>
2024-05-05 23:35:32 -0300

    Layer e construção do modelo de pedido

---
+++ lambda/orders/ordersFunction.ts
@@ -0,0 +1,52 @@
+import { DynamoDB } from "aws-sdk"
+import { Order, OrderRepository } from "/opt/nodejs/ordersLayer"
+import { Product, ProductRepository } from "/opt/nodejs/productsLayer"
+import * as AWSXRay from "aws-xray-sdk"
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda"
+import { OrderProductResponse, OrderRequest } from "/opt/nodejs/ordersApiLayer"
+
+AWSXRay.captureAWS(require("aws-sdk"))
+
+const ordersDdb = process.env.ORDERS_DDB!
+const productsDdb = process.env.PRODUCTS_DDB!
+
+const ddbClient = new DynamoDB.DocumentClient()
+
+const orderRepository = new OrderRepository(ddbClient, ordersDdb)
+const productRepository = new ProductRepository(ddbClient,productsDdb)
+
+export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult>{
+    const method = event.httpMethod
+    const apiRequestId = event.requestContext.requestId
+    const lambdaRequestId = context.awsRequestId
+
+    console.log(`API Gateway RequestId: ${apiRequestId} - LambdaRequestId: ${lambdaRequestId}`)
+
+    if(method === 'GET'){
+        if(event.queryStringParameters){
+            const email = event.queryStringParameters!.email
+            const orderId = event.queryStringParameters!.orderId
+            if(email){
+                if(orderId){
+                    //Get one order from an user
+                } else {
+                    //Get all orders from an user
+                }
+            }
+        } else {
+            //Get all orders
+        }
+
+    }else if (method === 'POST'){
+        console.log('POST /orders')
+    } else if (method === 'DELETE') {
+        console.log('DELETE /orders')
+        const email = event.queryStringParameters!.email
+        const orderId = event.queryStringParameters!.orderId
+    }
+
+    return {
+        statusCode: 400,
+        body: 'Bad request'
+    }
+}
--- lib/ordersApp-stack.ts
+++ lib/ordersApp-stack.ts
@@ -33,6 +33,10 @@ export class OrdersAppStack extends cdk.Stack {
         const ordersLayerArn = ssm.StringParameter.valueForStringParameter(this, "OrdersLayerVersionArn")
         const ordersLayer = lambda.LayerVersion.fromLayerVersionArn(this, "OrdersLayerVersionArn", ordersLayerArn)
 
+        //Orders API Layer
+        const ordersApiLayerArn = ssm.StringParameter.valueForStringParameter(this, "OrdersApiLayerVersionArn")
+        const ordersApiLayer = lambda.LayerVersion.fromLayerVersionArn(this, "OrdersApiLayerVersionArn", ordersApiLayerArn)
+
         //Products Layer
         const productsLayerArn = ssm.StringParameter.valueForStringParameter(this, "ProductsLayerVersionArn")
         const productsLayer = lambda.LayerVersion.fromLayerVersionArn(this, "ProductsLayerVersionArn", productsLayerArn)
@@ -49,7 +53,7 @@ export class OrdersAppStack extends cdk.Stack {
                     PRODUCTS_DDB: props.productsDdb.tableName,
                     ORDERS_DDB: ordersDdb.tableName
                 },
-                layers: [ordersLayer, productsLayer],
+                layers: [ordersLayer, productsLayer, ordersApiLayer],
                 tracing: lambda.Tracing.ACTIVE,
                 insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_119_0
         })
