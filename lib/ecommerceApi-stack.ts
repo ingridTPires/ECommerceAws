1ede61dc1f393338414f7893cef8b2f88c9d00b0
IngridTPires <ingrid.teixeirap@gmail.com>
2024-05-07 23:35:46 -0300

    Validação de requisição na criação do pedido

--- lib/ecommerceApi-stack.ts
+++ lib/ecommerceApi-stack.ts
@@ -43,9 +43,36 @@ export class ECommerceApiStack extends cdk.Stack {
 
         const productsAdminIntegration = new apigateway.LambdaIntegration(props.productsAdminHandler)
 
-        productsResource.addMethod("POST", productsAdminIntegration)
+        const productRequestValidator = new apigateway.RequestValidator(this, "ProductRequestValidator", {
+            restApi: api,
+            requestValidatorName: "Product request validator",
+            validateRequestBody: true
+        })
+        const productModel = new apigateway.Model(this, "ProductModel", {
+            modelName: "ProductModel",
+            restApi: api,
+            schema: {
+                type: apigateway.JsonSchemaType.OBJECT,
+                properties:{
+                    productName: { type: apigateway.JsonSchemaType.STRING },
+                    code: { type: apigateway.JsonSchemaType.STRING },
+                    model: { type: apigateway.JsonSchemaType.STRING },
+                    productUrl: { type: apigateway.JsonSchemaType.STRING },
+                    price: { type: apigateway.JsonSchemaType.NUMBER },
+                },
+                required: [ "productName", "code" ]
+            }
+        })
 
-        productIdResource.addMethod("PUT", productsAdminIntegration)
+        productsResource.addMethod("POST", productsAdminIntegration, {
+            requestValidator: productRequestValidator,
+            requestModels: { "application/json": productModel }
+        })
+
+        productIdResource.addMethod("PUT", productsAdminIntegration, {
+            requestValidator: productRequestValidator,
+            requestModels: { "application/json": productModel }
+        })
 
         productIdResource.addMethod("DELETE", productsAdminIntegration)
     }
@@ -76,6 +103,27 @@ export class ECommerceApiStack extends cdk.Stack {
         })
 
         //POST /orders
-        ordersResource.addMethod("POST", ordersIntegration)
+        const orderRequestValidator = new apigateway.RequestValidator(this, "OrderRequestValidator", {
+            restApi: api,
+            requestValidatorName: "Order request validator",
+            validateRequestBody: true
+        })
+        const orderModel = new apigateway.Model(this, "OrderModel", {
+            modelName: "OrderModel",
+            restApi: api,
+            schema: {
+                type: apigateway.JsonSchemaType.OBJECT,
+                properties:{
+                    email: { type: apigateway.JsonSchemaType.STRING },
+                    productIds: { type: apigateway.JsonSchemaType.ARRAY, minItems: 1, items: { type: apigateway.JsonSchemaType.STRING } },
+                    payment: { type: apigateway.JsonSchemaType.STRING, enum: ["CASH", "DEBIT_CARD", "CREDIT_CARD" ] }
+                },
+                required: [ "email", "productIds", "payment" ]
+            }
+        })
+        ordersResource.addMethod("POST", ordersIntegration, {
+            requestValidator: orderRequestValidator,
+            requestModels: { "application/json": orderModel }
+        })
     }
 }
