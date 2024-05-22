bc31cc213b166d761216ae251b61f8cd17ed5d64
IngridTPires <ingrid.teixeirap@gmail.com>
2024-05-21 22:51:19 -0300

    Criação da fila e inscrição no tópico SNS

--- lib/ordersApp-stack.ts
+++ lib/ordersApp-stack.ts
@@ -6,6 +6,8 @@ import * as ssm from 'aws-cdk-lib/aws-ssm'
 import * as sns from 'aws-cdk-lib/aws-sns'
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions'
 import * as iam from 'aws-cdk-lib/aws-iam'
+import * as sqs from "aws-cdk-lib/aws-sqs"
+import * as lambdaEventSource from "aws-cdk-lib/aws-lambda-event-sources"
 import { Construct } from 'constructs'
 
 interface OrdersAppStackProps extends cdk.StackProps {
@@ -129,5 +131,12 @@ export class OrdersAppStack extends cdk.Stack {
                 })
             }
         }))
+
+        const orderEventsQueue = new sqs.Queue(this, "OrderEventsQueue", {
+            queueName: "order-events",
+            enforceSSL: false,
+            encryption: sqs.QueueEncryption.UNENCRYPTED
+        })
+        ordersTopic.addSubscription(new subs.SqsSubscription(orderEventsQueue))
     }
 }
