0e153a6a3647c72478c3cb5dcd6f5515dfc6eac7
IngridTPires <ingrid.teixeirap@gmail.com>
2024-06-02 22:12:31 -0300

    TTL e dynamoDB Stream para eventos de timeout

--- lambda/invoices/invoiceEventsFunction.ts
+++ lambda/invoices/invoiceEventsFunction.ts
@@ -28,7 +28,10 @@ export async function handler(event: DynamoDBStreamEvent, context: Context): Pro
       } else if (record.eventName === 'MODIFY') {
 
       } else if (record.eventName === 'REMOVE') {
-
+            if(record.dynamodb!.OldImage!.pk.S === '#transaction') {
+                console.log('Invoice transaction event received')
+                promises.push(processExpiredTransaction(record.dynamodb!.OldImage!))
+            }
       }
     })
 
@@ -57,4 +60,20 @@ async function createEvent(invoiceImage: {[key: string]: AttributeValue}, eventT
         }
     }).promise()
     return
+}
+
+async function processExpiredTransaction(invoiceTransactionImage: {[key: string]: AttributeValue}): Promise<void> {
+    const transactionId = invoiceTransactionImage.sk.S!
+    const connectionId = invoiceTransactionImage.connectionId.S!
+
+    console.log(`TransactionId: ${transactionId} - ConnectionId> ${connectionId}`)
+
+    if(invoiceTransactionImage.transactionStatus.S === 'INVOICE_PROCESSED') {
+        console.log('Invoice processed')
+    } else {
+        console.log(`Invoice import failed - Status ${invoiceTransactionImage.transactionStatus.S}`)
+        await invoiceWSService.sendInvoiceStatus(transactionId, connectionId, 'TIMEOUT')
+
+        await invoiceWSService.disconnectClient(connectionId)
+    }
 }
